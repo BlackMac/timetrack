a34ee146eaec5d49377b71edc9a50ee3e9feb061
Stefan Lange-Hegermann <lange-hegermann@netzquadrat.de>
2009-03-25 12:17:22 +0100

    split other controllers

--- download.php
+++ download.php
@@ -12,23 +12,6 @@ if ($_GET['d']=="y") {
 	echo $daemon;
 	exit;
 }
-?>
-<!DOCTYPE html>
 
-<html lang="en">
-<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<title>Daemon downloaden</title>
-	<meta name="generator" content="TextMate http://macromates.com/">
-	<meta name="author" content="">
-</head>
-<body>
-<h1>Daemon herunterladen</h1>
-<p>
-	Der daemon ist fertig konfiguriert und muss nur so installiert werden, dass er beim Starten
-	des Systems immer läuft!
-</p>
-	<a href="<?php echo $_SERVER['REQUEST_URI'] ?>&d=y">Download</a>
-</body>
-</html>
+include "views/download.phtml";
 
