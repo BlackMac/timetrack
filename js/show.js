a64efa2fac88446662e0fa544991bf5e5322d8a6
leachiM2k <leachiM2k@leachiM2k.de>
2009-03-26 00:58:49 +0100

    added new field for username/password-change

--- js/show.js
+++ js/show.js
@@ -2,3 +2,8 @@ $('correct_action_link').addEvent('click', function() {
 		$('correct_widow').toggleClass('visible');
 		$('correction_value').focus();
 });
+
+$('changeuserdata_action_link').addEvent('click', function() {
+		$('changeuserdata_widow').toggleClass('visible');
+		$('changeuserdata_user').focus();
+});
