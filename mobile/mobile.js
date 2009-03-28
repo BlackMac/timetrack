4276e09b2e627c04939ddd5f511362ff72dfade7
Stefan Lange-Hegermann <stefan@imac.stefanlh.de>
2009-03-28 01:10:04 +0100

    scrolling the page is not possible anymore

--- mobile/mobile.js
+++ mobile/mobile.js
@@ -3,3 +3,5 @@ $$('.expressform').addEvent('submit', function(evnt) {
 	var ajaxparams = {h: evnt.target.getElement('input[name=h]').get('value') , d: evnt.target.getElement('button[name=d]').get('value') };
 	var ajax = new Request.HTML({url:'../log.php', onComplete: function() { location.reload(); } }).get(ajaxparams);
 }.bindWithEvent());
+
+document.addEventListener("touchmove", function(e){e.preventDefault()}, false);
