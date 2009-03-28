3f08f44833ad0aee66cac5e190d318a26b4d5f9c
Stefan Lange-Hegermann <stefan@imac.stefanlh.de>
2009-03-28 02:41:22 +0100

    cache manifest

---
+++ mobile/cache-manifest.php
@@ -0,0 +1,15 @@
+<?php
+header('Content-type: text/cache-manifest');
+//echo '#'.microtime()."\n";
+?>
+CACHE MANIFEST
+# v0.1
+style.css
+mobile.js
+mootools-yui-compressed.js
+
+../img/mobile/button_green.png
+../img/mobile/button_red.png
+../img/mobile/infogradient.png
+../img/mobile/topgradient.png
+../img/mobile/wait.gif.png
