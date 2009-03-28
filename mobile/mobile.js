00b46a5aa1123cac81a5d45c5e3bb347b318de12
Stefan Lange-Hegermann <stefan@imac.stefanlh.de>
2009-03-28 01:31:40 +0100

    added spinner screen

--- mobile/mobile.js
+++ mobile/mobile.js
@@ -1,7 +1,14 @@
+var wait_div=new Element('div', {'class':'wait_div'});
+
 $$('.expressform').addEvent('submit', function(evnt) {
+	wait_div.injectTop(document.body);
 	evnt.stop();
 	var ajaxparams = {h: evnt.target.getElement('input[name=h]').get('value') , d: evnt.target.getElement('button[name=d]').get('value') };
 	var ajax = new Request.HTML({url:'../log.php', onComplete: function() { location.reload(); } }).get(ajaxparams);
 }.bindWithEvent());
 
 document.addEventListener("touchmove", function(e){e.preventDefault()}, false);
+
+/* preload spinner */
+new Element('img', {'src':'../img/mobile/wait.gif'});
+//var spinnercache=new Asset.image('../img/mobile/wait.gif');
