8ef4846bfb296c7d33175645fe88edf9db325ee7
Michael Rotmanov <rotmanov@sipgate.de>
2009-05-05 13:12:28 +0200

    added modification comments to cache manifest

--- mobile/cache-manifest.php
+++ mobile/cache-manifest.php
@@ -3,16 +3,30 @@ header('Content-type: text/cache-manifest');
 $path_info=pathinfo($_SERVER['REQUEST_URI']);
 
 $url='http://'.$_SERVER['SERVER_NAME'].$path_info['dirname'].'/';
+$comment = "";
+$filelist = "";
+
+$files = array(
+    "style.css",
+    "mobile.js",
+    "mootools-yui-compressed.js",
+    "../images/navigator_buttons.gif",
+    "../img/mobile/button_green.png",
+    "../img/mobile/button_red.png",
+    "../img/mobile/infogradient.png",
+    "../img/mobile/topgradient.png",
+    "../img/mobile/wait.gif",
+    );
 //echo '#'.microtime()."\n";
+
+foreach($files as $file) {
+  if(!file_exists($file)) continue;
+  $comment .= "#" . $file . " - size: " . filesize($file) . " - time: " . filectime($file) . "\n"; 
+  $filelist .= $url . $file . "\n";
+}
 ?>
 CACHE MANIFEST
 # v0.1
-<?php echo $url; ?>style.css
-<?php echo $url; ?>mobile.js
-<?php echo $url; ?>mootools-yui-compressed.js
+<?php echo $comment; ?>
 
-<?php echo $url; ?>../img/mobile/button_green.png
-<?php echo $url; ?>../img/mobile/button_red.png
-<?php echo $url; ?>../img/mobile/infogradient.png
-<?php echo $url; ?>../img/mobile/topgradient.png
-<?php echo $url; ?>../img/mobile/wait.gif.png
+<?php echo $filelist; ?>
