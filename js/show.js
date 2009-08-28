3c45fc7c83d72d48824aa63af29ab6fa8ba6e732
Michael Rotmanov <rotmanov@sipgate.de>
2009-08-28 12:45:49 +0200

    Fixed negative time and added 60 minutes reloading

--- js/show.js
+++ js/show.js
@@ -8,14 +8,22 @@ $('changeuserdata_action_link').addEvent('click', function() {
 		$('changeuserdata_pass').focus();
 });
 
-var endofday = new Date();
-endofday.set('Time', day_diff * 1000);
+var countdownTimer;
+var endofday = new Date().set('Time', day_diff * 1000);
+var pageLoad = $time();
 
 function countdownDayDiff() {
 
 	var now = new Date();
 
-	if(now.diff(endofday, 'day') != 0) return;
+	if(now.diff(endofday, 'day') != 0 || laststateIn != "1") {
+		$clear(countdownTimer);
+		return;
+	}
+
+	if($time() - pageLoad > 60 * 60 * 1000) {
+		location.href = location.href;
+	}
 
 	var diff_second = now.diff(endofday, 'second');
 	var diff_hour = (diff_second / 60 / 60).toInt();
@@ -23,9 +31,17 @@ function countdownDayDiff() {
 	var diff_minute = (diff_second / 60).toInt();
 	diff_second = diff_second - diff_minute*60;
 
-	$('day_diff_hour').set('text', diff_hour);
-	$('day_diff_minute').set('text', (diff_minute<10 ? '0' : '') + diff_minute);
-	$('day_diff_seconds').set('text', (diff_second<10 ? '0' : '') + diff_second);
+	if(diff_second < 0 || diff_minute < 0 || diff_hour < 0) {
+		$('day_diff').removeClass('negative');
+		$$('.day_diff_minus').setStyle('display', 'none');
+		diff_hour = Math.abs(diff_hour);
+		diff_minute = Math.abs(diff_minute);
+		diff_second = Math.abs(diff_second);
+	}
+
+	$$('.day_diff_hour').set('text', diff_hour);
+	$$('.day_diff_minute').set('text', (diff_minute<10 ? '0' : '') + diff_minute);
+	$$('.day_diff_seconds').set('text', (diff_second<10 ? '0' : '') + diff_second);
 	
 };
 
@@ -36,7 +52,7 @@ window.addEvent('domready', function() {
 		size: {x: 600, y: 105}
 	});
 
-	countdownDayDiff.periodical(1000);
+	countdownTimer = countdownDayDiff.periodical(1000);
 
 	SqueezeBox.assign($$('a.editorLink'), {
 		parse: 'rel',
