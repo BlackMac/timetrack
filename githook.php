c186569686f2008947274b60e7be849abb74ef65
Stefan Lange-Hegermann <lange-hegermann@sipgate.de>
2009-12-02 09:12:51 +0100

    better githook output

--- githook.php
+++ githook.php
@@ -3,7 +3,7 @@
 <html lang="en">
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<title>untitled</title>
+	<title>GitHook</title>
 	<meta name="generator" content="TextMate http://macromates.com/">
 	<meta name="author" content="Stefan Lange-Hegermann">
 	<!-- Date: 2009-12-02 -->
