e70527f430f700c24908566ce3927060c26b2be6
Stefan Lange-Hegermann <lange-hegermann@sipgate.de>
2009-12-02 09:11:36 +0100

    better githook output

--- githook.php
+++ githook.php
@@ -1,6 +1,21 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<title>untitled</title>
+	<meta name="generator" content="TextMate http://macromates.com/">
+	<meta name="author" content="Stefan Lange-Hegermann">
+	<!-- Date: 2009-12-02 -->
+</head>
+<body>
+	<pre>
 <?php
 	$file=fopen('logs/githook.txt','w');
 	fwrite($file, date("l dS of F Y h:i:s A")."\n");
 	fclose($file);
-	exec('git pull');
+	passthru('git pull');
 ?>
+	</pre>
+</body>
+</html>
