eb9e40da9642f5d2b767ac2e3d6421c2e08c8b18
Stefan Lange-Hegermann <lange-hegermann@sipgate.de>
2009-12-02 09:08:46 +0100

    added githook.txt to gitignore

--- githook.php
+++ githook.php
@@ -1,6 +1,6 @@
 <?php
 	$file=fopen('logs/githook.txt','w');
-	fwrite($file, date("l dS of F Y h:i:s A"));
+	fwrite($file, date("l dS of F Y h:i:s A")."\n");
 	fclose($file);
 	exec('git pull');
 ?>
